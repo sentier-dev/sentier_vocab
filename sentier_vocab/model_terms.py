d38033273f21ddb0a72552717d6b3d0bacdecee5
Chris Mutel <cmutel@gmail.com>
2024-10-08 07:06:11 +0200

    Add first set of model terms

---
+++ sentier_vocab/model_terms.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import skosify
+from loguru import logger
+from rdflib import Graph
+
+filepath = Path(__file__).parent / "data" / "model-terms.ttl"
+
+
+def ModelTerms():
+    logger.info("Reading input TTL file {}", filepath.absolute())
+    graph = Graph().parse(filepath)
+    logger.info("Creating reciprocal relations")
+    skosify.infer.skos_topConcept(graph)
+    skosify.infer.skos_hierarchical(graph, narrower=True)
+    skosify.infer.skos_transitive(graph, narrower=True)
+
+    output_path = filepath.with_suffix(".reciprocal.ttl")
+    logger.info("Writing output TTL file {}", output_path)
+    graph.serialize(destination=output_path)
+    return filepath.with_suffix(".ttl")
+
+
+if __name__ == "__main__":
+    ModelTerms()
