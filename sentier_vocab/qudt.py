f7619256e8ead12371ca0f4a229f1791a44939f2
Chris Mutel <cmutel@gmail.com>
2024-10-09 20:42:01 +0200

    Reorg

--- sentier_vocab/qudt.py
+++ sentier_vocab/qudt.py
@@ -19,7 +19,7 @@ QUDTV = Namespace("http://qudt.org/vocab/")
 QK = QUDTV.quantitykind
 
 
-vocab_data_dir = Path(__file__).parent / "data"
+vocab_data_dir = Path(__file__).parent / "input"
 selected_fp = vocab_data_dir / "selected-quantity-kinds.json"
 extra_concepts_data = vocab_data_dir / "extra-data.ttl"
 qudt_patches_data = vocab_data_dir / "qudt-patches.ttl"
@@ -79,7 +79,7 @@ class QUDT:
         if not filename.endswith(".ttl"):
             filename += ".ttl"
         if not dirpath:
-            dirpath = vocab_data_dir
+            dirpath = Path(__file__).parent / "output"
         output_fp = Path(dirpath) / filename
         self.graph.serialize(destination=output_fp)
         return output_fp
@@ -338,5 +338,43 @@ class QUDT:
                 pass
 
 
+def add_quantity_kinds_to_graph(
+    input_ttl: Path,
+    qudt_ttl: Path,
+) -> Path:
+    """
+    The `input_ttl` concept scheme was written by hand (!), but we can make it more useful by
+    positioning each input concept in the QUDT quantity kind hierarchy.
+    """
+    output_ttl = input_ttl.with_suffix(".supplemented" + input_ttl.suffix)
+    input_graph = Graph().parse(input_ttl)
+    qudt = Graph().parse(qudt_ttl)
+
+    qudt_qk_mapping = {
+        s: o
+        for s, v, o in qudt.triples((None, QUDTS.hasQuantityKind, None))
+        if o.startswith("https://vocab.sentier.dev/units/quantity-kind/")
+        and s.startswith("https://vocab.sentier.dev/units/unit/")
+    }
+    qudt_d_mapping = {
+        s: o
+        for s, v, o in qudt.triples((None, QUDTS.hasDimensionVector, None))
+        if o.startswith("http://qudt.org/vocab/dimensionvector/")
+        and s.startswith("https://vocab.sentier.dev/units/unit/")
+    }
+
+    for s, v, o in input_graph.triples((None, SKOS.exactMatch, None)):
+        if o.startswith("https://vocab.sentier.dev/units/unit/"):
+            input_graph.add((s, QUDTS.hasQuantityKind, qudt_qk_mapping[o]))
+            input_graph.add((s, QUDTS.hasDimensionVector, qudt_d_mapping[o]))
+
+    input_graph.serialize(destination=output_ttl)
+    return output_ttl
+
+
 if __name__ == "__main__":
     QUDT().write_graph()
+    add_quantity_kinds_to_graph(
+        Path(__file__).parent / "input" / "simapro.ttl",
+        Path(__file__).parent / "output" / "qudt-sentier-dev.ttl"
+    )
