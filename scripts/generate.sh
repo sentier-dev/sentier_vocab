c1108600d2282f7f924fd008ed0d60238a9d12e7
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-10-09 13:15:43 +0200

    scripts/generate.sh: download input files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # This script generates all output data from the input files in this repository.
 #
-# If executed without arguments, it executes all generators:
+# If executed without arguments, it downloads the required external files and
+# executes all generators:
 #
 #     $ scripts/generate.sh
 #
-# The `generate` sub-commands can be used to execute individual stages:
+# The `fetch` and `generate` sub-commands can be used to execute individual
+# stages:
 #
+#     $ scripts/generate.sh fetch
 #     $ scripts/generate.sh generate nace
 set -euo pipefail
 
@@ -28,9 +31,11 @@ GENERATOR_ARGS=(
 )
 
 main() {
-    [[ "$#" -eq 0 ]] && set -- generate
+    [[ "$#" -eq 0 ]] && set -- all
     local cmd=$1; shift
     case "$cmd" in
+    all) fetch; generate;;
+    fetch) fetch "$@";;
     generate) generate "$@";;
     *) usage;;
     esac
@@ -42,9 +47,25 @@ Usage: $0 [CMD [ARG...]]
 
 Commands:
 
+    [all]
+    fetch
     generate [MODULE...]
 EOF
     return 1
+}
+
+fetch() {
+    [[ "$#" -eq 0 ]] || usage
+    echo == CN_2024.rdf.zip ==
+    curl \
+        --silent --show-error \
+        --cookie-jar cookies.txt --output /dev/null \
+        --data 'email=public%40showvoc.eu&password=showvoc' \
+        'https://showvoc.op.europa.eu/semanticturkey/it.uniroma2.art.semanticturkey/st-core-services/Auth/login'
+    curl \
+        --cookie cookies.txt --output CN_2024.rdf.zip \
+        'https://showvoc.op.europa.eu/semanticturkey/it.uniroma2.art.semanticturkey/st-core-services/Download/getFile?fileName=CN_2024.rdf.zip&ctx_project=ESTAT_Combined_Nomenclature%2C_2024_(CN_2024)'
+    unzip -u CN_2024.rdf.zip
 }
 
 generate() {
