330fbd52b822da1ae4eba30cea4ce16ba22c17c5
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-10-10 21:21:42 +0200

    generate and deploy same ci-cd wflow
    one workflow file to generate the ttls, and deploy the files to the
    server

--- sentier_vocab/custom_products.py
+++ sentier_vocab/custom_products.py
@@ -7,15 +7,21 @@ from sentier_vocab.add_terms import add_custom_terms
 from sentier_vocab.input.custom_products import CUSTOM_PRODUCTS_DATA
 
 created_ttls = []
+
+
 def add_custom_products():
-    out_file = add_custom_terms(CUSTOM_PRODUCTS_DATA, "https://vocab.sentier.dev/products/", "custom-products")
-    created_ttls.append({"graph": "https://vocab.sentier.dev/products/",
-                         "ttl_file": str(out_file.name)})
+    out_file = add_custom_terms(
+        CUSTOM_PRODUCTS_DATA, "https://vocab.sentier.dev/products/", "custom-products"
+    )
+    created_ttls.append(
+        {"graph": "https://vocab.sentier.dev/products/", "ttl_file": str(out_file.name)}
+    )
     logger.info(f"Added {out_file} to outputs")
-    
 
 
 if __name__ == "__main__":
     add_custom_products()
-    with Path("new_ttls.json").open(mode="w", encoding="utf-8") as f:
+    with Path(Path(__file__).parent / "output" / "new_ttls.json").open(
+        mode="w", encoding="utf-8"
+    ) as f:
         f.write(json.dumps(created_ttls, indent=2))
