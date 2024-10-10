e233d074a93ccca1866f079c89bd98def3fcc8bb
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-10-10 20:52:18 +0200

    create json file with custom products
    The custom product ttl file ref is added to a json when executing the
    custom_products.py module
    
    *
    
    Signed-off-by: Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@noreply.users.github.com>

--- sentier_vocab/custom_products.py
+++ sentier_vocab/custom_products.py
@@ -1,10 +1,21 @@
+import json
+from pathlib import Path
+
+from loguru import logger
+
 from sentier_vocab.add_terms import add_custom_terms
 from sentier_vocab.input.custom_products import CUSTOM_PRODUCTS_DATA
 
-
+created_ttls = []
 def add_custom_products():
-    add_custom_terms(CUSTOM_PRODUCTS_DATA, "https://vocab.sentier.dev/products/", "custom-products")
+    out_file = add_custom_terms(CUSTOM_PRODUCTS_DATA, "https://vocab.sentier.dev/products/", "custom-products")
+    created_ttls.append({"graph": "https://vocab.sentier.dev/products/",
+                         "ttl_file": str(out_file.name)})
+    logger.info(f"Added {out_file} to outputs")
+    
 
 
 if __name__ == "__main__":
     add_custom_products()
+    with Path("new_ttls.json").open(mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(created_ttls, indent=2))
